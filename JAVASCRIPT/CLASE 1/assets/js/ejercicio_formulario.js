3fc95b9e190daefff6aef2def4c0f7e55c7c252b
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2020-07-12 12:04:29 -0400

    ejemplos basicos de javascript

---
+++ JAVASCRIPT/CLASE 1/assets/js/ejercicio_formulario.js
@@ -0,0 +1,21 @@
+// Esta es la forma como podemos acceder a un elemento del HTML
+// y asignarlo a una variable para trabajar con el dato que contenga
+let nombre = document.getElementById("nombre").value;
+console.log(nombre);
+
+let miFormulario = document.getElementById("miFormulario");
+let nombreEventoFormulario = "submit";
+
+
+// Esta una forma de crear funciones en js
+function imprimirDatos(evento) {
+    evento.preventDefault();// Instruccion para detener el funcionamiento por defecto del evento
+    console.log("Se disparo el evento Enviar")
+
+    let nombre = document.getElementById("nombre").value;
+    console.log("Nombre: " + nombre);
+}
+
+//imprimirDatos(); // Ejemplo de como ejecutar una funcion
+
+miFormulario.addEventListener(nombreEventoFormulario, imprimirDatos);
